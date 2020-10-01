f5161268e0187432de048b197eaf2916de2f14a5
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2020-10-01 18:23:54 -0400

    Added Marvin

---
+++ morethanmarvin.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+###################################
+# WHAT IS IN THIS EXAMPLE?
+#
+# This bot listens in one channel and reacts to every text message.
+###################################
+
+import asyncio
+import logging
+import os
+import sys
+from dotenv import load_dotenv
+from poll_results import get_polls
+
+load_dotenv('secret.env')
+
+import pykeybasebot.types.chat1 as chat1
+from pykeybasebot import Bot
+
+logging.basicConfig(level=logging.DEBUG)
+
+if "win32" in sys.platform:
+    # Windows specific event-loop policy
+    asyncio.set_event_loop_policy(
+        asyncio.WindowsProactorEventLoopPolicy()  # type: ignore
+    )
+
+
+async def handler(bot, event):
+    if event.msg.content.type_name != chat1.MessageTypeStrings.TEXT.value:
+        return
+    if event.msg.content.type_name != chat1.MessageTypeStrings.TEXT.value:
+        return
+    if event.msg.content.text.body == "!pollresult":
+        channel = event.msg.channel
+        msg_id = event.msg.id
+        conversation_id = event.msg.conv_id
+        polls = get_polls()
+        await bot.chat.send(conversation_id, polls)
+    if event.msg.content.text.body == "!test":
+        channel = event.msg.channel
+        msg_id = event.msg.id
+        conversation_id = event.msg.conv_id
+        msg = 'PASS!'
+        await bot.chat.send(conversation_id, msg)
+    # channel = event.msg.channel
+    # msg_id = event.msg.id
+    # await bot.chat.react(channel, msg_id, ":clap:")
+
+listen_options = {
+    "local": False,
+    "wallet": False,
+    "dev": False,
+    "hide-exploding": False,
+    "convs": True,
+    "filter_channel": {"name": "morethanbits", "topic_name": "test", "members_type": "team"},
+    "filter_channels": None,
+}
+
+bot = Bot(username="morethanmarvin", paperkey=os.environ.get('KEYBASE_PAPERKEY'), handler=handler, home_path='./morethanmarvin')
+
+
+asyncio.run(bot.start(listen_options=listen_options))
---
+++ poll_results.py
@@ -0,0 +1,46 @@
+from rcp import get_poll_data
+from prettytable import PrettyTable
+
+
+def get_polls():
+    x = PrettyTable()
+
+    td = get_poll_data(
+        "https://www.realclearpolitics.com/epolls/2020/president/us/general_election_trump_vs_biden-6247.html"
+    )
+    pa = get_poll_data("https://www.realclearpolitics.com/epolls/2020/president/pa/pennsylvania_trump_vs_biden-6861.html")
+    # ky = get_poll_data("https://www.realclearpolitics.com/epolls/2020/president/ky/kentucky_trump_vs_biden-6915.html")
+
+
+    # x.field_names = list(td[0]["data"][0].keys())
+    # x.align = "l"
+    # x.add_row(['NATIONAL',"" ,"" ,"" ,"" ,"" ,""])
+
+    message = "```\n"
+
+    for row in td[0]["data"]:
+        if row['Poll'] == 'RCP Average':
+            message += f'National:\n' \
+                       f'Real Clear Politics Average:\n' \
+                       f'Date: {row["Date"]}\n' \
+                       f'Biden: {row["Biden (D)"]}  ' \
+                       f'Trump: {row["Trump (R)"]}  ' \
+                       f'Spread: {row["Spread"]}\n\n'
+
+    x.add_row(['PENNSYLVANIA', '', '', '', '', '', ''])
+    for row in pa[0]["data"]:
+        if row['Poll'] == 'RCP Average':
+            message += f'Pennsylvania:\n' \
+                       f'Real Clear Politics Average:\n' \
+                       f'Date: {row["Date"]}\n' \
+                       f'Biden: {row["Biden (D)"]}  ' \
+                       f'Trump: {row["Trump (R)"]}  ' \
+                       f'Spread: {row["Spread"]}'
+
+
+    message += "\n```"
+    return message
+
+
+if __name__ == '__main__':
+    print(get_polls())
