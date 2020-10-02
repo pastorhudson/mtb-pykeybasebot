d2e76476a6a75be4c0d7790dfce829dc60d0698d
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2020-10-02 17:19:26 -0400

    Added Solenium

--- botcommands/get_screenshot.py
+++ botcommands/get_screenshot.py
@@ -1,12 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome('../chromedriver/chromedriver')
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+
+driver = webdriver.Chrome('../chromedriver/chromedriver', options=chrome_options)
+
 driver.get("http://www.python.org")
 assert "Python" in driver.title
 elem = driver.find_element_by_name("q")
 elem.clear()
 elem.send_keys("pycon")
 elem.send_keys(Keys.RETURN)
+print(driver.title)
+print(driver.name)
 assert "No results found." not in driver.page_source
 driver.close()
