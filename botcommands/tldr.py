bbd25c3f998e85f49bb1dae0fe1b5db5b202a2d3
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2020-10-01 22:24:24 -0400

    added observations to tldr

--- botcommands/tldr.py
+++ botcommands/tldr.py
@@ -1,17 +1,21 @@
 from smmryAPI.smmryapi import SmmryAPI
 import os
 from dotenv import load_dotenv
-
+import random
 
 load_dotenv('../secret.env')
 
 
+
 def get_tldr(url):
     tldr = ""
-
+    observations = ["I'm sorry I'm such a failure.",
+                    "I'm so sorry you have to read all these words.",
+                    "I didn't even read the tl;dr.",
+                    "Now I'm stuck remembering this useless article forever. I hope it was worth it."]
     smmry = SmmryAPI(os.environ.get('SMMRY_API_KEY'))
     s = smmry.summarize(url, sm_length=3, sm_keyword_count=12)
-    tldr = "\n".join([f"Here's my tl;dr I could only reduce it by {s.sm_api_content_reduced}.\nI'm sorry I'm such a failure.", "```",
+    tldr = "\n".join([f"Here's my tl;dr I could only reduce it by {s.sm_api_content_reduced}.\n{random.choice(observations)}", "```",
                       str(s), "```"])
     return tldr
 
