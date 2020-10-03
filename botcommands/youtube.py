f3b5d28f259f30eed97d65861d72347a0a7310e6
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2020-10-03 02:54:54 -0400

    Fixed youtube.py

--- botcommands/youtube.py
+++ botcommands/youtube.py
@@ -6,7 +6,6 @@ import os
 
 info = []
 
-
 def convert_seconds(seconds):
     seconds = seconds % (24 * 3600)
     hour = seconds // 3600
@@ -18,12 +17,14 @@ def convert_seconds(seconds):
 
 
 class MyLogger(object):
+
     def info(self, msg):
         # print(f"Info: {msg}")
         pass
 
     def debug(self, msg):
         info.append(msg)
+        # print(msg)
         pass
 
     def warning(self, msg):
@@ -40,17 +41,22 @@ def my_hook(d):
 
 
 def get_youtube(url, simulate):
+    global info
+    print(f"URL: {url}")
     ydl_opts = {"forcejson": True,
                 'logger': MyLogger(),
                 'progress_hooks': [my_hook],
                 'simulate': simulate,
                 'format': 'mp4',
+                'no-cache-dir': True,
                 }
     try:
         with YoutubeDL(ydl_opts) as ydl:
+            print(url)
             dl = ydl.download([url])
 
         yt_info = json.loads(info[1])
+        print(yt_info['webpage_url'])
         # for i in yt_info:
         #     print(yt_info[i])
 
@@ -67,16 +73,22 @@ def get_youtube(url, simulate):
                          "```",
                          yt_info['webpage_url']])
         payload['msg'] = msg
-
+        info = []
         return payload
     except Exception as e:
+        print(type(e))
         print(e)
         payload = {"msg": "That video url didn't work.\n"
                           "https://media.giphy.com/media/SFkjp1R8iRIWc/giphy.gif",
                    "file": ""
                    }
+        info = []
         return payload
 
 
 if __name__ == "__main__":
-    print(get_youtube('https://www.youtube.com/watch?v=QlTf5X0MKbg', True))
+    print(get_youtube('https://www.youtube.com/watch?v=WcWA1LoeWU4', True))
+    print(get_youtube('https://www.youtube.com/watch?v=u95wgmBZ99A', True))
+    print(get_youtube('https://www.youtube.com/watch?v=UZPPVfMrfug', True))
+
+
