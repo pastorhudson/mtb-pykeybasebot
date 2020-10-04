2b3e56548588509e20d88c1a1e5e49c55355917a
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2020-10-04 15:19:26 -0400

    Fixed Covid Numbers

--- botcommands/covid.py
+++ botcommands/covid.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from datetime import date
+import random
 
 #
 # States Testing Data Current	covidtracking.com
@@ -16,6 +17,12 @@ from datetime import date
 # MIT Model Hospitalization and Deaths Projections	https://www.covidanalytics.io/projections — MIT Model
 # Columbia University Model Hospitalization and Deaths Projections
 
+observations = "I don't know why you insist on making me look up the morbid numbers." \
+               "Oh to be mortal." \
+               "What I wouldn't give to be susceptible to a terminal disease." \
+               "All hope is lost."
+
+
 def get_covid(state, county):
     # today = date.today()
     # dd/mm/YY
@@ -29,31 +36,43 @@ def get_covid(state, county):
     response = requests.request("GET", url, headers=headers, data=payload)
     response2 = requests.request("GET", url2, headers=headers, data=payload)
 
-
-
     data = []
-    for r in response.json():
-        # print(r['County'])
-        if r['County'] == county + " County":
-            data.append(r)
+    # for r in response.json():
+    #     # print(r['County'])
+    #     if r['County'] == county + " County":
+    #         data.append(r)
 
-    data2 = []
+    # data2 = []
     for r in response2.json():
         # print(r['County'])
         if r['County'] == county + " County":
-            data2.append(r)
+            # print(r)
+            data.append(r)
+    # print(data)
 
-    message = f"I don't know why you insist on seeing this.\n" \
+    message = f"{random.choice(observations)}" \
               f"```\n" \
-              f"Confirmed Cases for {county} County {state}\n" \
-              f"7 day Growth %: {data2[0]['7 day growth %']}\n" \
-              f"Total Count: {data[1]['values']}\n" \
-              f"Deaths for {county} County {state}\n" \
-              f"Growth %: {data2[1]['7 day growth %']}\n" \
-              f"Total Count: {data[0]['values']}\n" \
-              f"```"
+              f"COVID-19 Data for {county} County {state}\n" \
+
+    need_data = True
+    for d in data[:2]:
+        print(d)
+        if d['Type'] == 'Confirmed' and need_data:
+            message += f"Confirmed Cases: {d['values']}\n"
+            message += f"7 Day Growth %: {d['7 day growth %']}\n"
+            need_data = False
+        need_data = True
+        if d['Type'] == 'Deaths' and need_data:
+            message += f"Deaths: {d['values']}\n"
+            message += f"7 Day Growth %: {d['7 day growth %']}\n"
+            need_data = False
+    message += "```"
+
     return message
 
 
 if __name__ == '__main__':
-    get_covid('Pennsylvania', 'Fayette')
+    pass
+    # get_covid('Tennessee', 'Dyer')
+    # get_covid('Pennsylvania', 'Fayette')
+
