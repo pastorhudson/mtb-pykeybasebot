beaadfbc1ae72210147691363c4e8093c214688c
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2020-10-05 20:35:07 -0400

    Fix for downloading other videos

--- botcommands/youtube.py
+++ botcommands/youtube.py
@@ -3,8 +3,15 @@ from __future__ import unicode_literals
 from youtube_dl import YoutubeDL
 import json
 import os
+from urllib.parse import urlparse
+
+
+def get_domain(url):
+    # print(urlparse(url).netloc)
+    return urlparse(url).netloc
 
 info = []
+
 
 def convert_seconds(seconds):
     seconds = seconds % (24 * 3600)
@@ -42,6 +49,46 @@ class MyLogger(object):
 def my_hook(d):
     if d['status'] == 'finished':
         print('Done downloading, now converting ...')
+
+
+def get_video(url, simulate):
+    domain = get_domain(url)
+    if domain == 'youtube.com' or domain == 'youtu.be':
+        info = []
+        return get_youtube(url, simulate)
+
+    else:
+        info = []
+        return get_other_video(url, simulate)
+
+
+def get_other_video(url, simulate):
+    global info
+    ydl_opts = {"forcejson": True,
+                'logger': MyLogger(),
+                'progress_hooks': [my_hook],
+                'simulate': simulate,
+                'format': 'mp4',
+                'no-cache-dir': True,
+                }
+    with YoutubeDL(ydl_opts) as ydl:
+        dl = ydl.download([url])
+    yt_info = info[0]
+    payload = {"title": yt_info["fulltitle"],
+               "author": yt_info["uploader"],
+               "file": f'{os.path.abspath("./")}/{yt_info["_filename"]}',
+               "duration": convert_seconds(yt_info["duration"]),
+               'url': yt_info['webpage_url']
+               }
+    msg = "\n".join(["```", yt_info["fulltitle"],
+                     f"Author: {yt_info['uploader']}",
+                     f"Duration: {convert_seconds(yt_info['duration'])}",
+                     "```",
+                     yt_info['webpage_url']])
+    payload['msg'] = msg
+    info = []
+
+    return payload
 
 
 def get_youtube(url, simulate):
@@ -86,14 +133,17 @@ def get_youtube(url, simulate):
                    "file": ""
                    }
         info = []
+
         return payload
 
 
 if __name__ == "__main__":
     # print(get_youtube('https://www.youtube.com/watch?v=WcWA1LoeWU4', True))
     # print(get_youtube('https://www.youtube.com/watch?v=u95wgmBZ99A', True))
-    print(get_youtube('https://www.youtube.com/watch?v=UZPPVfMrfug', True))
+    # print(get_youtube('https://www.youtube.com/watch?v=UZPPVfMrfug', True))
     # print(get_youtube('https://www.dailymotion.com/video/x7wl5ns', False))
+    print(get_video("https://twitter.com/realDonaldTrump/status/1313267615083761665", True))
+    # print(get_video("https://youtu.be/R3kkpVbOcsA", True))
     # print(get_youtube('https://www.cnn.com/2020/10/02/politics/kellyanne-conway-positive-for-covid-19/index.html', True))
 
 
