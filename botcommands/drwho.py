b91a31e715398662c580a90aa90b9104d6fa3eef
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2020-10-13 11:08:37 -0400

    added episode search to drwho.py

--- botcommands/drwho.py
+++ botcommands/drwho.py
@@ -1,17 +1,42 @@
 import requests
 import random
 import json
+from fuzzywuzzy import fuzz
 
-
-def get_drwho(ep_number):
-    url = f'https://api.catalogopolis.xyz/v1/episodes/{ep_number}'
-
-    response = requests.request("GET", url, headers=None, data=None)
-
+def is_int(s):
     try:
-        ep = response.json()
-    except json.decoder.JSONDecodeError:
-        return f"There is no episode: {ep_number}.\nAnd you said you were a Dr Who Fan. . . :expressionless:"
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+def get_drwho(ep):
+
+    if is_int(ep):
+        url = f'https://api.catalogopolis.xyz/v1/episodes/{ep}'
+
+        response = requests.request("GET", url, headers=None, data=None)
+
+        try:
+            ep_data = response.json()
+        except json.decoder.JSONDecodeError:
+            return f"There is no episode: {ep}.\nAnd you said you were a Dr Who Fan. . . :expressionless:"
+    else:
+        url = f'https://api.catalogopolis.xyz/v1/episodes'
+        response = requests.request("GET", url, headers=None, data=None)
+
+        last_ratio = 0
+        for episode in response.json():
+            try:
+                # print(episode['title'])
+                ratio = (fuzz.ratio(episode['title'], ep))
+                if ratio > last_ratio:
+                    ep_data = episode
+                    last_ratio = ratio
+            except Exception as e:
+                print(e)
+
 
     observations = [
         "Now I'm watching TV. . . ",
@@ -19,13 +44,14 @@ def get_drwho(ep_number):
         "I miss Rose. . ."
     ]
     msg = f"{random.choice(observations)}```"
-    msg += f"Title: {ep['title']}\n" \
-           f"Air Date: {ep['originalAirDate']}\n" \
-           f"Run Time: {ep['runtime']}\n" \
+    msg += f"Title: {ep_data['title']}\n" \
+           f"Episode ID: {ep_data['id']}\n" \
+           f"Air Date: {ep_data['originalAirDate']}\n" \
+           f"Run Time: {ep_data['runtime']}\n" \
            f"```"
 
     return msg
 
 
 if __name__ == "__main__":
-    print(get_drwho(78055))
+    print(get_drwho("Bad wolf"))
