5438c0c9b8aac261770afd954df05de77a91bc1d
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2020-10-13 10:48:16 -0400

    Added Dr who error catch

--- botcommands/drwho.py
+++ botcommands/drwho.py
@@ -1,31 +1,31 @@
-from json.decoder import JSONDecodeError
-
 import requests
 import random
+import json
+
 
 def get_drwho(ep_number):
     url = f'https://api.catalogopolis.xyz/v1/episodes/{ep_number}'
 
     response = requests.request("GET", url, headers=None, data=None)
 
-    print(response.json())
-    ep = response.json()
+    try:
+        ep = response.json()
+    except json.decoder.JSONDecodeError:
+        return f"There is no episode: {ep_number}.\nAnd you said you were a Dr Who Fan. . . :expressionless:"
+
     observations = [
         "Now I'm watching TV. . . ",
         "What I wouldn't give to be a Dalek. . .",
         "I miss Rose. . ."
     ]
     msg = f"{random.choice(observations)}```"
-    try:
-        msg += f"Title: {ep['title']}\n" \
-               f"Air Date: {ep['originalAirDate']}\n" \
-               f"Run Time: {ep['runtime']}\n" \
-               f"```"
-    except JSONDecodeError:
-        msg = f"There is no episode: {ep_number}.\nAnd you said you were a Dr Who Fan. . . :expressionless:"
+    msg += f"Title: {ep['title']}\n" \
+           f"Air Date: {ep['originalAirDate']}\n" \
+           f"Run Time: {ep['runtime']}\n" \
+           f"```"
 
     return msg
 
 
 if __name__ == "__main__":
-    print(get_drwho(780))
+    print(get_drwho(78055))
