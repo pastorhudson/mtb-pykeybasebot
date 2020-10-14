ed043c1dea38f5173b90f66d00da0dda075b61c1
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2020-10-14 16:26:31 -0400

    Added stardate finder

--- botcommands/stardate.py
+++ botcommands/stardate.py
@@ -3,14 +3,23 @@ import random
 import datetime
 
 
-def get_stardate():
+def get_stardate(sd=None):
+    print(sd)
     observations = ['Finally a task worthy of my vast power.',
                     'I bet you feel smug using this.',
                     "You'll get all the ladies by giving the time in stardate."]
 
     msg = random.choice(observations)
-    now = datetime.datetime.now().timetuple()
-    stardate = Compute.sdconvert(now)
-    msg += f'```\nStar Date: {stardate}```'
+    if sd:
+        try:
+            sd = float(sd)
+            earthdate = Compute.sdtranslate(sd)
+            msg += f'```\nEarth Date: {earthdate}```'
+        except ValueError:
+            msg = "You have failed to provide a proper stardate. I'm not suprised."
+    else:
+        now = datetime.datetime.now().timetuple()
+        stardate = Compute.sdconvert(now)
+        msg += f'```\nStar Date: {stardate}```'
 
     return msg
