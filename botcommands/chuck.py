5b50ed87540939f466d7c46200ceacb0ab4ea701
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2020-10-14 21:43:41 -0400

    Added Chuck names and bomb

--- botcommands/chuck.py
+++ botcommands/chuck.py
@@ -1,23 +1,15 @@
 import requests
 import random
+from html import unescape
+
+def request_joke(joker):
+    url = f"http://api.icndb.com/jokes/random?firstName&lastName={joker}&?exclude=[explicit]&?escape=javascript"
+
+    response = requests.request("GET", url)
+    return unescape(response.json()['value']['joke'])
 
 
-def get_chuck(name=None, channel=None):
-    """{'owners': [{'uid': 'f4089cdf5fc8ebe433d5b9f49b66d619', 'username': 'pastorhudson', 'fullName': 'Ron Hudson'}],
-     'admins': [], 'writers': [{'uid': '7d368eae6a7292b4215ca46da021b919', 'username': 'sakanakami', 'fullName': 'Joe Eafrati'},
-      {'uid': 'b3543e75d25a5b5b45e551c3cccf0e19', 'username': 'ihuman', 'fullName': 'Ethan Connor'}],
-       'readers': [], 'bots': [{'uid': 'a5465087aede61be961a6bb3bf964f19', 'username': 'morethanmarvin',
-        'fullName': ''}, {'uid': 'ae0918c8e416811c7e6a4a737d39b119', 'username': 'marvn', 'fullName': ''}],
-         'restrictedBots': [{'uid': '228b5863ba9c6a97c2103f7d5f59bf19', 'username': 'rssbot', 'fullName': 'RSS Bot'}]}"""
-
-    observations = [
-        "This is very jouvinile.",
-        "I don't get it.",
-        "Does this make you happy? Because it doesn't make me happy.",
-        "I hope you don't plan on doing this all day. . .",
-        "Would you grow up already?",
-    ]
-
+def get_jokers(channel):
     joke_names = []
     for owners in channel['owners']:
         joke_names.append(owners['username'])
@@ -30,18 +22,43 @@ def get_chuck(name=None, channel=None):
     for readers in channel['readers']:
         if readers['username'] not in joke_names:
             joke_names.append(readers['username'])
+    return joke_names
 
-    todays_joker = random.choice(joke_names)
 
-    # url = "http://api.icndb.com/jokes/random?firstName=Ron&lastName=The%20Baptist"
-    url = f"http://api.icndb.com/jokes/random?firstName&lastName=@{todays_joker}&?exclude=[explicit]"
+def get_chuck(name=None, channel=None):
+    """{'owners': [{'uid': 'f4089cdf5fc8ebe433d5b9f49b66d619', 'username': 'pastorhudson', 'fullName': 'Ron Hudson'}],
+     'admins': [], 'writers': [{'uid': '7d368eae6a7292b4215ca46da021b919', 'username': 'sakanakami', 'fullName': 'Joe Eafrati'},
+      {'uid': 'b3543e75d25a5b5b45e551c3cccf0e19', 'username': 'ihuman', 'fullName': 'Ethan Connor'}],
+       'readers': [], 'bots': [{'uid': 'a5465087aede61be961a6bb3bf964f19', 'username': 'morethanmarvin',
+        'fullName': ''}, {'uid': 'ae0918c8e416811c7e6a4a737d39b119', 'username': 'marvn', 'fullName': ''}],
+         'restrictedBots': [{'uid': '228b5863ba9c6a97c2103f7d5f59bf19', 'username': 'rssbot', 'fullName': 'RSS Bot'}]}"""
 
-    response = requests.request("GET", url)
-    joke = response.json()
+    observations = [
+        "This is very jouvinile.",
+        "I can't even. . . ",
+        "I'm so sorry. . . I was created. . ."
+        "Does this make you happy? Because it doesn't make me happy.",
+        "I hope you don't plan on doing this all day. . .",
+        "Would you grow up already?",
+    ]
+    print(name)
+    joke = ""
+
+    if not name:
+        joke_names = get_jokers(channel)
+        todays_joker = "@" + random.choice(joke_names)
+        joke = request_joke(todays_joker)
+    elif name == 'bomb':
+        joke_names = get_jokers(channel)
+        print(joke_names)
+        for joker in joke_names:
+            print(joker)
+            joke += "\n".join([request_joke("@" + joker) + "\n"])
+    else:
+        joke = request_joke(name)
 
     msg = "`" + random.choice(observations) + '`\n\n'
-
-    msg += joke['value']['joke']
+    msg += joke
 
     return msg
 
