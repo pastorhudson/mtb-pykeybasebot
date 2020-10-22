49b64dc3552b1d7c29c872165eb8bb0bcffde211
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2020-10-22 11:34:32 -0400

    Added optional observation to till

--- botcommands/till.py
+++ botcommands/till.py
@@ -1,16 +1,24 @@
 import datetime
+# import random
 
 
-def get_till():
+def get_observation():
     observations = [
         'The end is coming.'
     ]
+    return observations[0]
+
+
+def get_till(observation=True):
+    msg = ""
+    if observation:
+        msg = get_observation()
 
     election = datetime.date(2020, 11, 3)
 
     days = election - datetime.date.today()
 
-    msg = f"\nThere are {days.days} days till the US Presidental Election."
+    msg = f"\nThere are `{days.days}` days till the US Presidental Election."
 
     return msg
 
