ec318fe9611ab7d6cb8a1cf762d86a61c4077324
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2020-10-22 23:11:02 -0400

    Added get_members.py

--- botcommands/get_members.py
+++ botcommands/get_members.py
@@ -14,3 +14,19 @@ def get_members(channel):
         if readers['username'] not in members:
             members.append(readers['username'])
     return members
+
+
+def get_user(channel):
+    members = []
+    for owners in channel['owners']:
+        members.append(owners['username'])
+    for admins in channel['admins']:
+        if admins['username'] not in members:
+            members.append(admins['username'])
+    for writers in channel['writers']:
+        if writers['username'] not in members:
+            members.append(writers['username'])
+    for readers in channel['readers']:
+        if readers['username'] not in members:
+            members.append(readers['username'])
+    return members
