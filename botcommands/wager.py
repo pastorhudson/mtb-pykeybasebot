fa7f93ff2bcce15b9c1aae8f3f0bc635ec45ee77
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2020-10-27 23:36:11 -0400

    Added wager.py

--- botcommands/wager.py
+++ botcommands/wager.py
@@ -1,72 +1,81 @@
-import datetime
-import pandas as pd
-import os
-import csv
 from crud import s
-from models import User, Team
+from datetime import datetime, timedelta
+from models import User, Team, Wager, Bet, Point
 
 
-def RepresentsInt(s):
-    try:
-        int(s)
-        return True
-    except ValueError:
-        return False
+def get_team_user(team_name, username):
+    team = s.query(Team).filter_by(name=team_name).first()
+    user = s.query(User).filter_by(username=username).first()
+
+    return team, user
 
 
-def get_score(channel_members, channel):
-    df = pd.read_csv(f"./storage/{channel}.csv")
-    members = channel_members
-    df.set_index('User', inplace=True)
-
-    df = df.groupby("User", sort=True)["Points"].sum().reset_index()
-    total = df.sort_values(by=['Points', 'User'], ascending=[False, True])
-    total_members = total[total.User.isin(members)]
-    msg = "```" + total_members.to_string(index=False) + "```"
-
+def get_wagers(team_name):
+    team = s.query(Team).filter_by(name=team_name).first()
+    wagers = team.get_wagers()
+    msg = f"Here's all the current wagers for {team_name}\n"
+    for wager in wagers:
+        msg += f'```Wager: `#{wager.id}` "{wager.description}"\n' \
+               f'Default Bet: {wager.points}\n' \
+               f'End Time: {wager.et()}```\n\n'
     return msg
 
 
-def make_wager(user, channel_members, channel, wager):
-    msg = ""
-    if RepresentsInt(wager.split(" ")[0]):
-        points_bet = wager.split(" ")[0]
-        print(len(points_bet))
-        msg = f"@{user} Has wagered `{points_bet}` points \n\n```{wager[3:]}```"
-        msg += "\nVote Against: :no_entry_sign:\n" \
-               "Vote For: :white_check_mark:"
+def make_wager(team_name, username, description, points, position,  minutes):
+    team, user = get_team_user(team_name, username)
+
+    existing_wager = team.wager_exists(description)
+    exists = False
+    for w in existing_wager:
+        if w.description == description:
+            exists = True
+            return f"Wager: `#{w.id}` {w.description} already exists.\n" \
+                   f"```Usage: !bet <#wager> <points> <True/False>```"
+
+    if not exists:
+        end_time = datetime.now() + timedelta(minutes=minutes)
+        bet = Bet(points=points, position=position)
+        wager = Wager(team_id=team.id, description=description, points=points, start_time=datetime.now(), end_time=end_time)
+        bet.wager = wager
+        user.bets.append(bet)
+        s.commit()
+        msg = f'Wager: `#{wager.id}`\n"{wager.description}"\n' \
+              f'Default Bet: {wager.points}\n' \
+              f'End Time: {wager.et()}'
+        s.close()
         return msg
+
+
+def make_bet(team_name, username, points, position, wager_id):
+    team, user = get_team_user(team_name, username)
+    wager = team.get_wager(wager_id)
+    if wager and not wager.is_closed:
+        if bet := wager.already_bet(user):
+            return f"You've already placed a bet that Wager: `#{wager.id}` is `{bet.position}` for `{bet.points}`.\n" \
+                   f"This wager will end at {wager.et()}"
+        else:
+            bet = Bet(points=points, position=True)
+            bet.wager = wager
+            user.bets.append(bet)
+            s.commit()
+            msg = f'Wager: `#{wager.id}`\n"{wager.description}"\n' \
+                   f'Default Bet: {wager.points}\n' \
+                   f'End Time: {wager.et()}'
+            s.close()
+            return msg
+
     else:
-        return "I didn't get a wager in the proper format\n" \
-               "Usage: <points> <Wager Description>\n" \
-               '!wager 25 Daredevil is Blind'
-
-
-def has_points():
-
-    return True
-
-
-def get_users():
-    t = Team(name='morethanbits')
-    u = User(username='marvn')
-    t.users = [u]
-    s.add(t)
-    s.add(u)
-    s.commit()
-    print(u.teams)
-    #
-    users_list = s.query(User.username).all()
-    for u in users_list:
-        print(u.username)
-
-    # print(users)
-    s.close()
+        return f'Wager: `#{wager.id}` "{wager.description}" is closed for betting.'
 
 
 if __name__ == "__main__":
-    # channel_members = {'owners': [{'uid': 'a5465087aede61be961a6bb3bf964f19', 'username': 'morethanmarvin', 'fullName': ''}, {'uid': 'f4089cdf5fc8ebe433d5b9f49b66d619', 'username': 'pastorhudson', 'fullName': 'Ron Hudson'}], 'admins': [], 'writers': [], 'readers': [], 'bots': [], 'restrictedBots': []}
-    # channel = "morethanbits"
-    # wager = "25 Amy Coney Barrette will be confirmed tonight"
-    # print(make_wager("pastorhudson", channel_members, channel, wager))
-    get_users()
+    # print(make_wager('morethanmarvin,pastorhudson', 'pastorhudson', 'My db is lit', 24, 30))
+    print(get_wagers('morethanmarvin,pastorhudson'))
+    # print(make_bet(team_name='morethanmarvin,pastorhudson', username='pastorhudson', points=23, position=True, wager_id=5))
+    # print(make_wager(team_name='morethanmarvin,pastorhudson',
+    #                  username='pastorhudson',
+    #                  description="The moon is flat.",
+    #                  points=56,
+    #                  position=True,
+    #                  minutes=60
+    #                  ))
