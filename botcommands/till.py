97e04d8152a344bf7e3bce1f1a88a038e7b7abd3
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2020-11-10 11:32:43 -0500

    Fixed time logic for future dates

--- botcommands/till.py
+++ botcommands/till.py
@@ -36,7 +36,7 @@ def set_till(team_name, event_name, event_time):
     team = get_team(team_name)
     till_event = dateparser.parse(event_time, settings={'PREFER_DATES_FROM': 'future'})
     current_time = datetime.now(timezone.utc)
-    tills = team.tills.filter(Till.name == event_name).all()
+    tills = team.tills.filter(Till.name == event_name).filter(Till.event > current_time).all()
     if len(tills) > 0:
         return f"There is already a till set for {event_name}\n{tills[0]}"
     till = Till(team_id=team.id, name=event_name, event=till_event)
@@ -47,6 +47,6 @@ def set_till(team_name, event_name, event_time):
 
 if __name__ == "__main__":
     # print(get_till(team_name='morethanmarvin,pastorhudson'))
-    print(set_till(team_name='morethanmarvin,pastorhudson', event_name='Yule', event_time='8:30 AM on Monday, December 21'))
-    # print(set_till(team_name='morethanmarvin,pastorhudson', event_name='Newyears', event_time='January 1'))
+    # print(set_till(team_name='morethanmarvin,pastorhudson', event_name='Yule', event_time='8:30 AM on Monday, December 21'))
+    print(set_till(team_name='morethanmarvin,pastorhudson', event_name='US Presidential Election', event_time='November 3 2024'))
 
