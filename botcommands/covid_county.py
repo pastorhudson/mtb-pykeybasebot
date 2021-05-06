c049ff886b30a9ad12565a6438b8b896fd57d3a4
Ron Hudson <pastorhudson@users.noreply.github.com>
2021-05-06 16:08:24 -0400

    fix path on us-counties.csv

--- botcommands/covid_county.py
+++ botcommands/covid_county.py
@@ -5,7 +5,7 @@ import json
 
 def get_county(state, county):
 
-    with open('/storage/us-counties.csv', mode='r') as csv_file:
+    with open('/app/storage/us-counties.csv', mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
             # print(row)
