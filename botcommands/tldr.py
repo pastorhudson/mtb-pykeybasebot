0d2f345bec1071d4c9c75fec401c055ccbd549f5
Ron Hudson <pastorhudson@users.noreply.github.com>
2021-10-18 13:01:03 -0400

    handle too short txt

--- botcommands/tldr.py
+++ botcommands/tldr.py
@@ -1,23 +1,27 @@
 from smmryAPI.smmryapi import SmmryAPI, SmmryAPIException
 import os
 from newspaper import Article
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import random
 import requests
 
-load_dotenv('../secret.env')
+# load_dotenv('../secret.env')
 
 
 def get_smmry_txt(url):
-    # txt = get_text(url)
-    api_key = os.environ.get('SMMRY_API_KEY')
-    # headers = {'content-type': 'application/json'}
-    payload = {'sm_api_input': get_text(url),
-               'sm_length': 3,
-               'sm_keyword_count': 12}
-    surl = f"https://api.smmry.com/&SM_API_KEY={api_key}"
-    r = requests.post(surl, payload)
-    return r.json()
+    txt = get_text(url)
+    if len(txt) > 550:
+        api_key = os.environ.get('SMMRY_API_KEY')
+        # headers = {'content-type': 'application/json'}
+        payload = {'sm_api_input': get_text(url),
+                   'sm_length': 3,
+                   'sm_keyword_count': 12}
+        surl = f"https://api.smmry.com/&SM_API_KEY={api_key}"
+        r = requests.post(surl, payload)
+        return r.json()
+    else:
+        return {f'sm_api_content_reduced': f'Zero Percent because it was only {len(txt)} char',
+                'sm_api_content': txt}
 
 
 def get_tldr(url):
@@ -26,12 +30,11 @@ def get_tldr(url):
                     "I'm so sorry you have to read all these words.",
                     "I hope this makes you happy because I'm not.",
                     "Now I'm stuck remembering this useless article forever. I hope it was worth it."]
-    # smmry = SmmryAPI(os.environ.get('SMMRY_API_KEY'))
     try:
-        # s = smmry.summarize(url, sm_length=3, sm_keyword_count=12)
         s = get_smmry_txt(url)
         tldr = "\n".join([f"Here's my tl;dr I could only reduce it by {s['sm_api_content_reduced']}.\n{random.choice(observations)}", "```",
                       str(s['sm_api_content']), "```"])
+
     except SmmryAPIException:
         errors = ["You have burned out my eyes sending me page. I hope you're happy",
                         "This page is full of cancer and now I am full of cancer.",
@@ -52,6 +55,8 @@ def get_text(url=None):
         import nltk
         nltk.download('punkt')
         article.nlp()
+        # print(article.text)
+        # print(len(article.text))
 
     return article.text
 
@@ -60,4 +65,5 @@ if __name__ == "__main__":
     # print(get_tldr('https://www.chicagotribune.   com/coronavirus/ct-nw-hope-hicks-trump-covid-19-20201002-mdjcmul6pnajvg56zoxqrcnf5m-story.html'))
     # print(get_tldr('https://www.cnn.com/2021/10/18/politics/colin-powell-dies/index.html'))
     # print(get_tldr('https://www.cnn.com/2021/10/18/politics/joe-biden-democrats-economy-supply-chain-donald-trump-2022-midterms/index.html'))
-    print(get_tldr('https://www.cnn.com/2021/10/18/politics/joe-biden-democrats-economy-supply-chain-donald-trump-2022-midterms/index.html'))
+    # print(len(get_text('https://patch.com/pennsylvania/pittsburgh/consumer-alert-issued-pittsburgh-area-pizza-shop')))
+    print(get_tldr('https://patch.com/pennsylvania/pittsburgh/consumer-alert-issued-pittsburgh-area-pizza-shop'))
