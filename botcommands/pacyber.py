538e828b3184adbeaa54519332390afd6ea8d424
Ron Hudson <pastorhudson@users.noreply.github.com>
2023-04-20 18:58:05 -0400

    update GRADES

--- botcommands/pacyber.py
+++ botcommands/pacyber.py
@@ -1,13 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 import os
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 import unicodedata
-
+from datetime import datetime
 
 """ Copy and rename the secrets_env template in this repo to secrets.env.
     Add your own login and password for https://myschool.pacyber.org"""
-# load_dotenv('secret.env')
+load_dotenv('secret.env')
 
 """ Your login information below. It's probably best to store this in environment variables."""
 tbLogin = os.environ.get('TBLOGIN')
@@ -19,6 +19,7 @@ tbPassword = os.environ.get('TBPASSWORD')
 
 """ Internal URL that requires user to be logged in"""
 SCRAPEURL = 'https://myschool.pacyber.org/FEGradebook.aspx'
+# https://myschool.pacyber.org/FEGradebook.aspx
 
 """Here is where we setup headers to make it look like a browser"""
 headers = {
@@ -36,6 +37,63 @@ login_data = dict(__VIEWSTATE=None,
                   tbLogin=tbLogin,
                   tbPassword=tbPassword,
                   btLogin="")  # We'll populate this below after we initilize the session.
+
+
+def get_last_activity(event_target, s):
+    data = {
+        "ctl00$sm": f"ctl00$ContentPlaceHolder1$GradebookInfo1$updatePanelGridViewCourses | ctl00$ContentPlaceHolder1$GradebookInfo1$gvCourses$ctl{event_target}$MyRadioButton1",
+        "__EVENTTARGET": f"ctl00$ContentPlaceHolder1$GradebookInfo1$gvCourses$ctl{event_target}$MyRadioButton1",
+        f"ctl00$ContentPlaceHolder1$GradebookInfo1$gvCourses$ctl{int(event_target) -1}$SelectGroup": "MyRadioButton1",
+        f"ctl00$ContentPlaceHolder1$GradebookInfo1$gvCourses$ctl{event_target}$SelectGroup": "MyRadioButton1",
+    }
+    url = 'https://myschool.pacyber.org/FEGradebook.aspx'  # grade_url
+    r = s.get(url, headers=headers)  # This is where we make the first request to generate an authenticity_token
+    # print(r.content)
+    soup = BeautifulSoup(r.content,
+                         'html.parser')  # We're using Beautiful Soup to scrape the token form the page source
+    # """Here we are populating data dictionary with the scraped auth tokens"""
+    data['__VIEWSTATE'] = soup.find('input', attrs={'name': '__VIEWSTATE'})['value']
+    data['__VIEWSTATEGENERATOR'] = soup.find('input', attrs={'name': '__VIEWSTATEGENERATOR'})['value']
+    data['__EVENTVALIDATION'] = soup.find('input', attrs={'name': '__EVENTVALIDATION'})['value']
+    page = s.post(SCRAPEURL, data)
+    bs = BeautifulSoup(page.content, 'html.parser')
+
+    """ Grab the tables for the subjects and their corresponding info """
+    try:
+        activity_book_div = bs.find("div", {"id": "ctl00_ContentPlaceHolder1_GradebookInfo1_PanelCourseDetail"})
+        activity_book_table = activity_book_div.find('table', {"id": "ctl00_ContentPlaceHolder1_GradebookInfo1_gvAssignment"})
+        activity_book_rows = activity_book_table.findAll('tr')
+        cur_date = ""
+        last_date = ""
+        for row in activity_book_rows:
+            td = row.find_all('td')
+            row = [unicodedata.normalize("NFKD", i.text) for i in td]
+            """Get datetime for 'Aug 31, 2022'"""
+            try:
+                if last_date == "":
+                    last_date = datetime.strptime(row[-1:][0], '%b %d, %Y')
+                    cur_date = datetime.strptime(row[-1:][0], '%b %d, %Y')
+                else:
+                    cur_date = datetime.strptime(row[-1:][0], '%b %d, %Y')
+                if last_date < cur_date:
+                    last_date = cur_date
+                # print(f"Current Date: {cur_date} - "
+                #       f"Last Date: {last_date} - {row[-1:]}")
+            except Exception as e:
+                # print(e)
+                pass
+            # print(row[-1:])
+
+    except Exception as e:
+        return "Please Check your secrets.env and ensure your login and password are correctly set."
+    # print(last_date)
+    try:
+        days = datetime.today() - last_date
+        days = days.days
+        last_date = f"{last_date.strftime('%b %m, %Y')} ({days} days)"
+    except:
+        days = 'NA'
+    return last_date
 
 
 def get_academic_snapshot():
@@ -57,7 +115,9 @@ def get_academic_snapshot():
         page = s.get(SCRAPEURL)
 
         bs = BeautifulSoup(page.content, 'html.parser')
-
+        login_data['__VIEWSTATE'] = bs.find('input', attrs={'name': '__VIEWSTATE'})['value']
+        login_data['__VIEWSTATEGENERATOR'] = bs.find('input', attrs={'name': '__VIEWSTATEGENERATOR'})['value']
+        login_data['__EVENTVALIDATION'] = bs.find('input', attrs={'name': '__EVENTVALIDATION'})['value']
         """ Grab the tables for the subjects and their corresponding info """
         try:
             grade_book_table = bs.find("table", {"id": "ctl00_ContentPlaceHolder1_GradebookInfo1_gvCourses"})
@@ -70,17 +130,28 @@ def get_academic_snapshot():
         for row in grade_book_rows:
             td = row.find_all('td')
             row = [unicodedata.normalize("NFKD", i.text) for i in td]
+            last_activity = None
+            for t in td:
+                for tt in t.find_all('input'):
+                    activity_table_id = tt.get('id').split("_")[4][3:]
+
+                    last_activity = get_last_activity(activity_table_id, s)
             try:
+
                 msg += "\n".join([f"Subject: {row[2]}",
                                   f"Score: {row[5].split(' ')[0] or None}",
                                   f"Progress: {row[6]}",
+                                  f"Last Date: {last_activity}",
                                   "\n"])
-
             except IndexError:
                 pass
-
         return msg
 
 
 if __name__ == "__main__":
     print(get_academic_snapshot())
+    # day1 = 'Mar 10, 2023'
+    # day2 = 'Apr 13, 2023'
+    # first = datetime.strptime(day1, '%b %d, %Y')
+    # second = datetime.strptime(day2, '%b %d, %Y')
+    # print(first > second)
