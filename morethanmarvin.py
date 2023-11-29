0cfbb2421c8903506a9181611cba43ba6fad228e
Ron Hudson <pastorhudson@users.noreply.github.com>
2023-11-28 20:27:54 -0500

    add file download

--- morethanmarvin.py
+++ morethanmarvin.py
@@ -443,9 +443,12 @@ async def handler(bot, event):
                                 f"for trying to be cute.\n{instructions}")
 
     if str(event.msg.content.text.body).startswith("@marvn"):
+        logging.info("I'm triggering @marvn")
         conversation_id = event.msg.conv_id
         await bot.chat.react(conversation_id, event.msg.id, ":marvin:")
         if event.msg.content.type == "attachment":
+            logging.info("I got an attachment")
+
             message_id = event.msg.id
             channel = event.msg.conv_id
 
@@ -454,7 +457,7 @@ async def handler(bot, event):
             # Download the file
 
             await bot.download(channel, message_id, filename)
-            print(f"File downloaded: {filename}")
+            logging.info(f"File downloaded: {filename}")
         msg = get_chat(str(event.msg.content.text.body)[7:])
         await bot.chat.send(conversation_id, msg)
 
