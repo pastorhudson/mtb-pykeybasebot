485d016c4b42bd881b3914e9e083cdfad42ebbcc
Ron Hudson <pastorhudson@users.noreply.github.com>
2023-11-30 18:33:47 -0500

    add 11 pts per comment

--- botcommands/award_activity_points.py
+++ botcommands/award_activity_points.py
@@ -15,9 +15,9 @@ async def award_activity_points(event):
 
         team_name = event.msg.channel.name
         logging.info(f'Giving {event.msg.sender.username} 11 pts')
-        score = write_score(event.msg.sender.username, '@marvn', team_name, 11, description='sent msg')
-    if event.msg.content.type_name == 'reaction':
+        score = write_score('@marvn', event.msg.sender.username, team_name, 11, description='sent msg')
+    if event.msg.content.type_name == 'reaction' and event.msg.sender.username != '@marvn':
         team_name = event.msg.channel.name
         logging.info(f'Giving {event.msg.sender.username} 4 pts')
-        score = write_score(event.msg.sender.username, '@marvn', team_name, 4, description='sent msg')
+        score = write_score('@marvn', event.msg.sender.username, team_name, 4, description='sent msg')
     pass
