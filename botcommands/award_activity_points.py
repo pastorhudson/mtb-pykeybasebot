f9ebd287d4e1ab8183fd9262d653c14c3d565c30
Ron Hudson <pastorhudson@users.noreply.github.com>
2023-11-30 19:31:50 -0500

    fix marvn getting points

--- botcommands/award_activity_points.py
+++ botcommands/award_activity_points.py
@@ -12,10 +12,10 @@ async def award_activity_points(event):
 
         if str(event.msg.content.text.body).startswith("!"):
             score = write_score('@marvn', event.msg.sender.username, team_name, 3, description='sent msg')
-        elif event.msg.sender.username != '@marvn':
+        elif event.msg.sender.username != 'marvn':
             logging.info(f'Giving {event.msg.sender.username} {len(str(event.msg.content.text.body))}pts')
             score = write_score('@marvn', event.msg.sender.username, team_name, len(str(event.msg.content.text.body)), description='msg')
-    if event.msg.content.type_name == 'reaction' and event.msg.sender.username != '@marvn':
+    if event.msg.content.type_name == 'reaction' and event.msg.sender.username != 'marvn':
         team_name = event.msg.channel.name
         logging.info(f'Giving {event.msg.sender.username} 4 pts')
         score = write_score('@marvn', event.msg.sender.username, team_name, 4, description='react')
