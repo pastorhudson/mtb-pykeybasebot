d956d8fed8c70741790f0ea45402face0d63992a
Ron Hudson <pastorhudson@users.noreply.github.com>
2023-11-30 19:25:30 -0500

    clean up

--- botcommands/award_activity_points.py
+++ botcommands/award_activity_points.py
@@ -9,9 +9,10 @@ async def award_activity_points(event):
 
     if event.msg.content.type_name == "text":
         team_name = event.msg.channel.name
+
         if str(event.msg.content.text.body).startswith("!"):
             score = write_score('@marvn', event.msg.sender.username, team_name, 3, description='sent msg')
-        else:
+        elif event.msg.sender.username != '@marvn':
             logging.info(f'Giving {event.msg.sender.username} {len(str(event.msg.content.text.body))}pts')
             score = write_score('@marvn', event.msg.sender.username, team_name, len(str(event.msg.content.text.body)), description='msg')
     if event.msg.content.type_name == 'reaction' and event.msg.sender.username != '@marvn':
