999e8981f23c0c9dc9b71e8341ffea81a100dd7b
Ron Hudson <pastorhudson@users.noreply.github.com>
2023-12-04 17:02:59 -0500

    add logging to msg

--- morethanmarvin.py
+++ morethanmarvin.py
@@ -472,7 +472,7 @@ async def handler(bot, event):
         logging.info(event.msg.content.text)
         if event.msg.content.text.reply_to:
             logging.info("I have a reply")
-            original_msg = bot.chat.get(conversation_id, event.msg.content.reply_t)
+            original_msg = bot.chat.get(conversation_id, event.msg.content.text.reply_to)
             logging.info(original_msg)
         #     message_id = event.msg.id
         #     channel = event.msg.conv_id
