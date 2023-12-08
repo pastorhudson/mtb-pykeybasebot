7dfdb024cba59dbfdcab805b4946718b07385ed2
Ron Hudson <pastorhudson@users.noreply.github.com>
2023-12-08 16:35:43 -0500

    Add await to bot chat message in 'db_events.py'
    The 'run_db_events' function in 'db_events.py' now uses the await function when sending the bot chat test message to ensure the message is fully sent before proceeding. This enhances reliability of tests and improves event synchronization in the bot operations.

--- botcommands/db_events.py
+++ botcommands/db_events.py
@@ -7,5 +7,5 @@ from datetime import datetime
 
 async def run_db_events(bot):
     ron_marvn = '0000c3e1daf296e6c893a02f6ae2e39bbe99ecfbdc7bec6daccb3fd9efb0382d'
-    bot.chat.send(ron_marvn, f'Test Message! {datetime.utcnow()}')
+    mst = await bot.chat.send(ron_marvn, f'Test Message! {datetime.utcnow()}')
     logging.info("Running db_events")
