7adeb976e1d8b73a89512ec599ae79547b781a52
Ron Hudson <pastorhudson@users.noreply.github.com>
2023-12-08 16:31:46 -0500

    Add timestamped test message in 'db_events.py'
    A datetime function has been added to the 'run_db_events' function in 'db_events.py' to send a timestamped test message. This allows for improved tracking and debugging in the testing phase by providing a real-time timestamp for each test event generated.

--- botcommands/db_events.py
+++ botcommands/db_events.py
@@ -1,7 +1,11 @@
 import logging
+from datetime import datetime
+
 
 # from crud import s
 
 
 async def run_db_events(bot):
+    ron_marvn = '0000c3e1daf296e6c893a02f6ae2e39bbe99ecfbdc7bec6daccb3fd9efb0382d'
+    bot.chat.send(ron_marvn, f'Test Message! {datetime.utcnow()}')
     logging.info("Running db_events")
