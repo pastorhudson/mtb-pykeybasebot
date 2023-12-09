581a3301130a2d5d73256a93fb3b1704e05f66a5
Ron Hudson <pastorhudson@users.noreply.github.com>
2023-12-08 21:20:21 -0500

    Implement team logging and decrease sleep time in bot event function
    In botcommands/db_events.py, added functionality to query all teams and log their names with specific markers for certain conditions. In morethanmarvin.py, reduced the interval of executing the bot event function from 3600 sec to 15 sec to accelerate script operations. These changes together make for more comprehensive logging and a more responsive bot.

--- botcommands/db_events.py
+++ botcommands/db_events.py
@@ -1,11 +1,19 @@
 import logging
 from datetime import datetime
-
-
-# from crud import s
+from crud import s
+from models import Team
 
 
 async def run_db_events(bot):
     ron_marvn = '0000c3e1daf296e6c893a02f6ae2e39bbe99ecfbdc7bec6daccb3fd9efb0382d'
+    teams = s.query(Team).all()
+    for team in teams:
+        if team.name == ron_marvn:
+            logging.info("RON AND MARVN ACTION")
+        elif "," in team.name:
+            logging.info(f"Comma Team:{team.name}")
+        else:
+            logging.info(f"Real Team: {team.name}")
+    early_bird = None
     mst = await bot.chat.send(ron_marvn, f'Test Message! {datetime.utcnow()}')
     logging.info("Running db_events")
