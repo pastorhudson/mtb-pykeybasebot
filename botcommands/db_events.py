f6bed7b3cdc17a62892c1471fadbc8f812a54a13
Ron Hudson <pastorhudson@users.noreply.github.com>
2023-12-08 21:37:58 -0500

    Add query and logging for teams and users in bot event function
    Extended the functionality of `run_db_events` function in `botcommands/db_events.py`. The script now queries and logs users along with teams based on specific conditions. The changes contribute to a more informative log, making the bot's activity easier to monitor and debug.

--- botcommands/db_events.py
+++ botcommands/db_events.py
@@ -1,10 +1,15 @@
 import logging
 from datetime import datetime
+
+from sqlalchemy import func
+
 from crud import s
-from models import Team
+from models import Team, Point, User
 
 
 async def run_db_events(bot):
+    today = datetime.now().date()
+
     ron_marvn = '0000c3e1daf296e6c893a02f6ae2e39bbe99ecfbdc7bec6daccb3fd9efb0382d'
     teams = s.query(Team).all()
     for team in teams:
@@ -14,6 +19,11 @@ async def run_db_events(bot):
             logging.info(f"Comma Team:{team.name}")
         else:
             logging.info(f"Real Team: {team.name}")
+            # Query to get the first user who has points for today
+            user_id = s.query(Point).filter(func.date(Point.created_at) == today) \
+                .order_by(Point.created_at.asc()).first()
+            user = s.query(User).filter(User.id == user_id).first()
+            logging.info(f"User: {user}")
     early_bird = None
     mst = await bot.chat.send(ron_marvn, f'Test Message! {datetime.utcnow()}')
     logging.info("Running db_events")
