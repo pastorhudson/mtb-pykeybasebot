aefa1046c4e362db2bebb760579dd69988b4d767
Ron Hudson <pastorhudson@users.noreply.github.com>
2023-12-08 22:13:43 -0500

    Uncomment code in db_events.py
    The previously commented code in the db_events.py file has been uncommented. Now, queries to fetch all teams and to get the first user who has points for today are active. Exception logging has also been enabled to handle and notify any error occurring during these processes.

--- botcommands/db_events.py
+++ botcommands/db_events.py
@@ -11,23 +11,23 @@ async def run_db_events(bot):
     today = datetime.now().date()
 
     ron_marvn = '0000c3e1daf296e6c893a02f6ae2e39bbe99ecfbdc7bec6daccb3fd9efb0382d'
-    # try:
-    #     teams = s.query(Team).all()
-    #     for team in teams:
-    #         if team.name == ron_marvn:
-    #             logging.info("RON AND MARVN ACTION")
-    #         elif "," in team.name:
-    #             logging.info(f"Comma Team:{team.name}")
-    #         else:
-    #             logging.info(f"Real Team: {team.name}")
-    #             # Query to get the first user who has points for today
-    #             user_id = s.query(Point).filter(func.date(Point.created_at) == today) \
-    #                 .order_by(Point.created_at.asc()).first()
-    #             logging.info(f"user_id:{user_id}")
+    try:
+        teams = s.query(Team).all()
+        for team in teams:
+            if team.name == ron_marvn:
+                logging.info("RON AND MARVN ACTION")
+            elif "," in team.name:
+                logging.info(f"Comma Team:{team.name}")
+            else:
+                logging.info(f"Real Team: {team.name}")
+                # Query to get the first user who has points for today
+                user_id = s.query(Point).filter(func.date(Point.created_at) == today) \
+                    .order_by(Point.created_at.asc()).first()
+                logging.info(f"user_id:{user_id}")
     #             # user = s.query(User).filter(User.id == user_id).first()
     #             # logging.info(f"User: {user}")
     #     early_bird = None
     #     mst = await bot.chat.send(ron_marvn, f'Test Message! {datetime.utcnow()}')
-    # except Exception as e:
-    #     logging.info(e)
+    except Exception as e:
+        logging.info(e)
     logging.info("Running db_events")
