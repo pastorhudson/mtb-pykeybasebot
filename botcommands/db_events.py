ef79108fdc03c78bdfa19f9b334a4a6cf11992d6
Ron Hudson <pastorhudson@users.noreply.github.com>
2023-12-08 22:27:41 -0500

    Update datetime queries and logging in db_events.py
    The querying for datetime has been adjusted to use "tomorrow" rather than "today". This modification is expected to provide more accurate results for the intended purpose. Logging has been updated to log the point receiver only if a point is found, improving error handling and readability.

--- botcommands/db_events.py
+++ botcommands/db_events.py
@@ -1,5 +1,5 @@
 import logging
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from sqlalchemy import func
 
@@ -9,6 +9,7 @@ from models import Team, Point, User
 
 async def run_db_events(bot):
     today = datetime.now().date()
+    tomorrow = today + timedelta(days=1)
 
     ron_marvn = '0000c3e1daf296e6c893a02f6ae2e39bbe99ecfbdc7bec6daccb3fd9efb0382d'
     try:
@@ -21,9 +22,10 @@ async def run_db_events(bot):
             else:
                 logging.info(f"Real Team: {team.name}")
                 # Query to get the first user who has points for today
-                point = s.query(Point).filter(func.date(Point.created_at) == today) \
+                point = s.query(Point).filter(func.date(Point.created_at) == tomorrow) \
                     .order_by(Point.created_at.asc()).first()
-                logging.info(f"user:{point.point_receiver}")
+                if point:
+                    logging.info(f"user:{point.point_receiver}")
                 early_bird = point.point_receiver
     #     early_bird = None
     #     mst = await bot.chat.send(ron_marvn, f'Test Message! {datetime.utcnow()}')
