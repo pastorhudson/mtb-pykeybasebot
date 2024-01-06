d44b74bcdc81b2cf62c295df0104a57fc588b91f
Ron Hudson <pastorhudson@users.noreply.github.com>
2024-01-06 16:06:48 -0500

    Disable awarding points in bot commands
    The `write_score` function calls in `award_activity_points.py` have been disabled. This means the bot will no longer award points for sending messages or reactions. This change is likely temporary and could be part of debugging or refactoring efforts.

--- botcommands/award_activity_points.py
+++ botcommands/award_activity_points.py
@@ -11,12 +11,15 @@ async def award_activity_points(event):
         team_name = event.msg.channel.name
 
         if str(event.msg.content.text.body).startswith("!") or str(event.msg.content.text.body).startswith("http"):
-            score = write_score(event.msg.sender.username, 'marvn', team_name, 3, description='sent msg')
+            # score = write_score(event.msg.sender.username, 'marvn', team_name, 3, description='sent msg')
+            pass
         elif event.msg.sender.username != 'marvn':
             logging.info(f'Giving {event.msg.sender.username} {len(str(event.msg.content.text.body))}pts')
-            score = write_score(event.msg.sender.username, 'marvn', team_name, len(str(event.msg.content.text.body)), description='msg')
+            # score = write_score(event.msg.sender.username, 'marvn', team_name, len(str(event.msg.content.text.body)), description='msg')
+            pass
     if event.msg.content.type_name == 'reaction' and event.msg.sender.username != 'marvn':
         team_name = event.msg.channel.name
         logging.info(f'Giving {event.msg.sender.username} 4 pts')
-        score = write_score(event.msg.sender.username, 'marvn', team_name, 4, description='react')
+        # score = write_score(event.msg.sender.username, 'marvn', team_name, 4, description='react')
+        pass
     pass
