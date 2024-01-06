ae0350f01dac33bcb1b0869bdbc0e613c0404aca
Ron Hudson <pastorhudson@users.noreply.github.com>
2024-01-06 14:03:18 -0500

    Update logger and timings in db_events.py
    Enhanced the logging process in db_events.py to include 'Early Bird' and 'Time to post'. Included CompletedTasks in the query to filter completed 'morning_report' jobs. Timings for tasks have been updated to allow posts after 1:35pm rather than exactly at 1:35pm, enabling more flexibility.

--- botcommands/db_events.py
+++ botcommands/db_events.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 from sqlalchemy import func
 from crud import s
-from models import Team, Point, User
+from models import Team, Point, User, CompletedTasks
 
 
 async def run_db_events(bot):
@@ -29,7 +29,7 @@ async def run_db_events(bot):
                     .order_by(Point.created_at.asc()).first()
                 logging.info(point)
                 if point:
-                    logging.info(f"user:{point.point_receiver}")
+                    logging.info(f"Early Bird:{point.point_receiver}")
                     early_bird = point.point_receiver
 
                 else:
@@ -38,8 +38,12 @@ async def run_db_events(bot):
             #     logging.info("Yes, it's 5:23am America/New_York time.")
             # else:
             #     logging.info("No, it's not 5:23am America/New_York time.")
-            if now.hour == 13 and now.minute == 35:
-                logging.info("Yes, it's 1:35pm America/New_York time.")
+            if now.hour >= 13 and now.minute >= 35:
+                logging.info("Yes, it's Time to post! America/New_York time.")
+                completed = s.query(CompletedTasks).filter(func.date(CompletedTasks.completed_at == today) \
+                    .filter(func.date(CompletedTasks.task_name) == 'morning_report')) \
+                    .order_by(Point.created_at.asc()).first()
+                print(completed)
             else:
                 logging.info("No, it's not 1:35pm America/New_York time.")
     #     mst = await bot.chat.send(ron_marvn, f'Test Message! {datetime.utcnow()}')
