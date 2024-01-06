791cd4bece3e538cd5fed9f85575b3c1579cdd1c
Ron Hudson <pastorhudson@users.noreply.github.com>
2024-01-06 14:20:25 -0500

    Modified team name check and tidied-up logging
    Added specific team name check for 'morethanbits' to show relevant logging results. Removed extraneous comments and improved the logging of current time to enhance readability. Additionally, optimized the time comparison for post deadline purposes to ensure a correct condition is used.

--- botcommands/db_events.py
+++ botcommands/db_events.py
@@ -21,7 +21,7 @@ async def run_db_events(bot):
             elif "," in team.name:
                 # logging.info(f"Comma Team:{team.name}")
                 pass
-            else:
+            elif team.name == 'morethanbits':
                 logging.info(f"Real Team: {team.name}")
 
                 # Query to get the first user who has points for today
@@ -34,20 +34,16 @@ async def run_db_events(bot):
 
                 else:
                     logging.info(f"No early bird for {team.name} yet")
-            # if now.hour == 5 and now.minute == 23:
-            #     logging.info("Yes, it's 5:23am America/New_York time.")
-            # else:
-            #     logging.info("No, it's not 5:23am America/New_York time.")
-            logging.info(f"Right Now: {now_time.strftime}")
-            if now_time.hour >= 13 and now_time.minute >= 35:
-                logging.info("Yes, it's Time to post! America/New_York time.")
-                completed = s.query(CompletedTasks).filter(func.date(CompletedTasks.completed_at == today) \
-                    .filter(func.date(CompletedTasks.task_name) == 'morning_report')) \
-                    .order_by(Point.created_at.asc()).first()
-                print(completed)
-            else:
-                logging.info("No, it's not 1:35pm America/New_York time.")
-    #     mst = await bot.chat.send(ron_marvn, f'Test Message! {datetime.utcnow()}')
+
+                logging.info(f"Right Now: {now_time}")
+                if now_time.hour >= 13 and now_time.minute >= 35:
+                    logging.info("Yes, it's Time to post! America/New_York time.")
+                    completed = s.query(CompletedTasks).filter(func.date(CompletedTasks.completed_at == today) \
+                        .filter(func.date(CompletedTasks.task_name) == 'morning_report')) \
+                        .order_by(Point.created_at.asc()).first()
+                    print(completed)
+                else:
+                    logging.info("No, it's not 1:35pm America/New_York time.")
     except Exception as e:
         logging.info(e)
     logging.info("Running db_events")
