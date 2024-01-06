47f65db9e9b7b1f233ccef8d23c87d8e1ea88380
Ron Hudson <pastorhudson@users.noreply.github.com>
2024-01-06 13:30:03 -0500

    Add timezone support and logging for specific time checks
    Incorporated support for the 'America/New_York' timezone in bot command. Added additional logging conditions to identify if the current time is either 5:23 am or 1:35 pm, enhancing the ability to perform event-based checks.

--- botcommands/db_events.py
+++ botcommands/db_events.py
@@ -1,14 +1,14 @@
 import logging
 from datetime import datetime, timedelta
-
+from zoneinfo import ZoneInfo
 from sqlalchemy import func
-
 from crud import s
 from models import Team, Point, User
 
 
 async def run_db_events(bot):
     today = datetime.now().date()
+    now = datetime.now(ZoneInfo('America/New_York'))
     tomorrow = today + timedelta(days=1)
 
     ron_marvn = '0000c3e1daf296e6c893a02f6ae2e39bbe99ecfbdc7bec6daccb3fd9efb0382d'
@@ -34,6 +34,14 @@ async def run_db_events(bot):
 
                 else:
                     logging.info(f"No early bird for {team.name} yet")
+            # if now.hour == 5 and now.minute == 23:
+            #     logging.info("Yes, it's 5:23am America/New_York time.")
+            # else:
+            #     logging.info("No, it's not 5:23am America/New_York time.")
+            if now.hour == 13 and now.minute == 35:
+                logging.info("Yes, it's 1:35pm America/New_York time.")
+            else:
+                logging.info("No, it's not 1:35pm America/New_York time.")
     #     mst = await bot.chat.send(ron_marvn, f'Test Message! {datetime.utcnow()}')
     except Exception as e:
         logging.info(e)
