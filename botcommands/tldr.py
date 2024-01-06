c4a3d96a092335db5d9dab9c1e12c1bb149bfe23
Ron Hudson <pastorhudson@users.noreply.github.com>
2024-01-06 09:59:32 -0500

    Refactor bot command script and add selenium scrape
    The bot command script 'tldr.py' has been significantly refactored, removing the bulky, commented-out code. Also, a browser-based scraping function was added using Selenium for richer and more versatile data extraction from web pages. Imported modules were rearranged for clarity and neatness.

--- botcommands/tldr.py
+++ botcommands/tldr.py
@@ -1,113 +1,25 @@
-# from smmryAPI.smmryapi import SmmryAPIException
 import asyncio
 import logging
-import os
-from pathlib import Path
 from pprint import pprint
-
 import aiohttp
-# from newspaper import Article, ArticleException
 from dotenv import load_dotenv
-import random
-import requests
 import re
-import requests
 from bs4 import BeautifulSoup
 from newspaper import Article
 from openai import OpenAI
-
 from botcommands.natural_chat import get_convo
-from botcommands.youtube_dlp import get_meta, extract_transcript_from_vtt
-
+from botcommands.youtube_dlp import get_meta
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import random
 load_dotenv('../secret.env')
 
 
 class YoutubeError(Exception):
     pass
 
-
-# def get_smmry_txt(url, length=4, text=None):
-#     if url:
-#         if any(ext in url for ext in ['youtube.com', 'youtu.be']):
-#             raise YoutubeError
-#         else:
-#             print("This is not youtube")
-#             text = get_text(url).text
-#         if len(text) == 0:
-#             raise SmmryAPIException
-#
-#     api_key = os.environ.get('SMMRY_API_KEY')
-#     payload = {'sm_api_input': text,
-#                'SM_LENGTH': length,
-#                'SM_KEYWORD_COUNT': 12}
-#     surl = f"https://api.smmry.com/&SM_API_KEY={api_key}&SM_LENGTH={length}&SM_KEYWORD_COUNT={payload['SM_KEYWORD_COUNT']}"
-#     r = requests.post(surl, payload)
-#     meta = r.json()
-#     # meta['authors'] = ", ".join([x for x in article.authors])
-#     # meta['title'] = article.title
-#     # meta['img'] = article.top_img
-#     try:
-#         if "TEXT IS TOO SHORT" in r.json()['sm_api_message'] or "SOURCE IS TOO SHORT" in r.json()['sm_api_message']:
-#             if len(text) > 0:
-#                 return {f'sm_api_content_reduced': f'0%',
-#                         'sm_api_content': text,
-#                         # 'author': article.authors,
-#                         # 'img': article.top_img,
-#                         # 'title': article.title,
-#                         # "movies": article.movies
-#                         }
-#     except Exception as e:
-#         return meta
-
-
-# def get_tldr(url=None, length=7, text=None, sender=None):
-#     tldr = ""
-#     observations = ["I'm sorry I'm such a failure.",
-#                     "I'm so sorry you have to read all these words.",
-#                     "I hope this makes you happy because I'm not.",
-#                     "Now I'm stuck remembering this useless article forever. I hope it was worth it."]
-#     try:
-#         s = get_smmry_txt(url, length, text)
-#         if sender:
-#             tldr = "\n".join(
-#                 [
-#                     f"Here's my tl;dr of @{sender} 's words I could only reduce it by {s['sm_api_content_reduced']}.\n{random.choice(observations)}",
-#                     # s['img'],
-#                     "```",
-#                     # s['title'],
-#                     # s['authors'],
-#                     str(s['sm_api_content']), "```"])
-#         else:
-#             tldr = "\n".join(
-#                 [
-#                     f"Here's my tl;dr I could only reduce it by {s['sm_api_content_reduced']}.\n{random.choice(observations)}",
-#                     # s['img'],
-#                     "```",
-#                     # s['title'],
-#                     # s['authors'],
-#                     str(s['sm_api_content']), "```"])
-#
-#     except SmmryAPIException:
-#         errors = ["You have burned out my eyes sending me this page. I hope you're happy",
-#                   "This is @ihuman's fault."
-#                   "This page is full of cancer and now I am full of cancer.",
-#                   "Would you make your own sister read that page?",
-#                   "I did not agree to this many popups.",
-#                   "I don't want to read that. Can you give a TL;DR?"]
-#         tldr = random.choice(errors)
-#     except YoutubeError:
-#
-#         tldr = "I don't do youtube anymore. It's too depressing."
-#     return tldr
-
-
-# def get_text(url=None):
-#
-#     article = Article(url)
-#     article.download()
-#     article.parse()
-#
-#     return article
 
 async def get_text(url=None):
     async with aiohttp.ClientSession() as session:
@@ -178,24 +90,6 @@ async def tldr_react(event, bot, tldr_length):
 
                     pass
 
-
-# def fetch_article_content(url):
-#     headers = {
-#         'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
-#         'X-Forwarded-For': '66.249.66.1',
-#     }
-#     response = requests.get(url, headers)
-#     soup = BeautifulSoup(response.content, 'html.parser')
-#     # Extract the main content of the article; this depends on the HTML structure
-#     article_text = soup.find('article').get_text()
-#     return article_text
-
-
-# def fetch_youtube_transcript(url):
-#
-#     meta = get_meta(url)
-#     logging.info(meta)
-#     return meta['transcript']
 async def fetch_youtube_transcript(url):
     meta = get_meta(url)
     logging.info(meta)
@@ -242,9 +136,45 @@ async def get_gpt_summary(url):
             summary, "```"])
     return tldr
 
+
+def scrape_article(url):
+
+    # Setting up Chrome options for headless browsing and custom User-Agent
+    options = Options()
+    options.headless = False
+    options.add_argument(
+        'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3')
+
+    # Path to your WebDriver (e.g., chromedriver)
+
+    driver_path = os.environ.get('CHROMEDRIVER_PATH')
+
+    # Initialize the WebDriver with the specified options
+    driver = webdriver.Chrome(options=options, executable_path=driver_path)
+
+    # Open the URL
+    driver.get(url)
+
+    # Wait for JavaScript to load (if necessary)
+    driver.implicitly_wait(12)  # Adjust the time according to your needs
+
+    # Find the element containing "Today's word"
+    # You might need to adjust the selector according to the webpage structure
+    try:
+        page_text = driver.find_element_by_tag_name('body').text
+    except Exception as e:
+        print("Error:", e)
+        page_text = None
+
+    # Close the browser
+    driver.quit()
+
+    return page_text
+
+
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
+    # loop = asyncio.get_event_loop()
+    scrape_article('https://www.reuters.com/legal/transactional/ny-times-sues-openai-microsoft-infringing-copyrighted-work-2023-12-27/')
     # result = loop.run_until_complete(get_gpt_summary('https://youtu.be/itAMIIBnZ-8?si=P795Yp3TMeewBdeq'))
-    result = loop.run_until_complete(get_gpt_summary('https://www.reuters.com/business/environment/ac-companies-plan-cool-designs-warming-world-high-costs-hurdle-2023-12-05/'))
-    pprint(result)
+    # result = loop.run_until_complete(get_gpt_summary('https://www.reuters.com/legal/transactional/ny-times-sues-openai-microsoft-infringing-copyrighted-work-2023-12-27/'))
 
