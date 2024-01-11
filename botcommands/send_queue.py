cdb6ad011e1d0694a4e1cc1f2def84745a045a53
Ron Hudson <pastorhudson@users.noreply.github.com>
2024-01-11 15:17:38 -0500

    Add logging and message sending functionality to bot.
    Extended the botcommands' `process_message_queue` function in 'send_queue.py' to include a default "Hello World!" message and a detailed logging for each message processed. Fixed a minor bug that caused the wrong content to be sent as bot's chat message. These changes improve tracking of message processing and debug capabilities.

--- botcommands/send_queue.py
+++ botcommands/send_queue.py
@@ -7,6 +7,7 @@ from models import MessageQueue
 
 async def process_message_queue(bot):
     logging.info("Checking if we have messages")
+    await bot.chat.send('0000c3e1daf296e6c893a02f6ae2e39bbe99ecfbdc7bec6daccb3fd9efb0382d', "Hello World!")
 
     try:
         logging.info("Trying to look for messages")
@@ -14,10 +15,11 @@ async def process_message_queue(bot):
         logging.info(f"{len(messages)}")
         for message in messages:
             logging.info("Iterating messages")
+            logging.info(f"{message.message_id}: {message.status}")
             if message.status == 'PENDING':
                 logging.info(f"Message status: {message.status}")
                 message.mark_as_processing()
-                await bot.chat.send(message.destination, messages.message)
+                await bot.chat.send(message.destination, message.message)
                 message.mark_as_processing()
                 s.commit()
         logging.info("All messages processed")
