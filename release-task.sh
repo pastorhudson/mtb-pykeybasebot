28cb89d617aabbd615a698fbeba524bd078c1002
Ron Hudson <pastorhudson@users.noreply.github.com>
2024-01-11 23:37:40 -0500

    Add release-task script and update Procfile
    A new release-task script has been created to send a POST request. This script has also been added to the Procfile to run upon deployment. This additional process will allow direct messaging notifications to the user upon release.

---
+++ release-task.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e  # Stop the script on the first error
+
+# Your additional POST command
+curl -X POST --location "https://marvn.app/add_message" -H "Content-Type: application/json" -d '{"message": "@marvn Releasing", "sender": "Dokku", "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDUwMzQ0NjcsInVzZXIiOiJwYXN0b3JodWRzb24iLCJjb252ZXJzYXRpb25faWQiOiIwMDAwYzNlMWRhZjI5NmU2Yzg5M2EwMmY2YWUyZTM5YmJlOTllY2ZiZGM3YmVjNmRhY2NiM2ZkOWVmYjAzODJkIn0.vXs05VfuFlkD_p0K8bQo9GV5P87HLpgkpabL3eD3BcU"}'
