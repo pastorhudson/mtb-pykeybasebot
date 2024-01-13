8a1ff2fe9f6bcba7aeb403df1f1066987d183ef6
Ron Hudson <pastorhudson@users.noreply.github.com>
2024-01-12 21:56:41 -0500

    Remove webhook Keybase service from Procfile
    The webhook Keybase service and its related subprocesses have been removed from the Procfile. This update simplifies the web process in Procfile by running only the gunicorn service with the webhook bot app.

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
-web: python webhook_keybase_service.py & gunicorn webhook_bot:app --workers=2
+web: gunicorn webhook_bot:app --workers=2
 bot: python morethanmarvin.py
 release: ./release-task.sh
--- webhook_keybase_service.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-import os
-
-subprocess.call(args=['./keybase', '--home', './webhookbot', 'service', '--oneshot-username', os.environ.get('KEYBASE_BOTNAME'), '<', os.environ.get('KEYBASE_PAPERKEY')])
