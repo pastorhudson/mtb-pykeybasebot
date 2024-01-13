9cf77175473acbd035f27bb5e363fb3680ce8bae
Ron Hudson <pastorhudson@users.noreply.github.com>
2024-01-12 19:51:44 -0500

    Update sender in notification POST request
    Modified the sender field in the POST request of the release-task.sh script. Changed from the string "Dokku" to a URL "https://marvn.app" to make the source of the notification more explicit and meaningful.

--- release-task.sh
+++ release-task.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e  # Stop the script on the first error
 # Notify Keybase
-curl -X POST --location "https://marvn.app/add_message" -H "Content-Type: application/json" -d '{"message": "@marvn Releasing", "sender": "Dokku", "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDUwMzQ0NjcsInVzZXIiOiJwYXN0b3JodWRzb24iLCJjb252ZXJzYXRpb25faWQiOiIwMDAwYzNlMWRhZjI5NmU2Yzg5M2EwMmY2YWUyZTM5YmJlOTllY2ZiZGM3YmVjNmRhY2NiM2ZkOWVmYjAzODJkIn0.vXs05VfuFlkD_p0K8bQo9GV5P87HLpgkpabL3eD3BcU"}'
+curl -X POST --location "https://marvn.app/add_message" -H "Content-Type: application/json" -d '{"sender": "https://marvn.app", "message": "@marvn Releasing", "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MDUxMDg4NTksInVzZXIiOiJwYXN0b3JodWRzb24iLCJjb252ZXJzYXRpb25faWQiOiIwMDAwYzNlMWRhZjI5NmU2Yzg5M2EwMmY2YWUyZTM5YmJlOTllY2ZiZGM3YmVjNmRhY2NiM2ZkOWVmYjAzODJkIn0.gthNoLLjCyAsX_-SZtiNXHuBoxwRpa4s93lzPUyDT_A"}'
