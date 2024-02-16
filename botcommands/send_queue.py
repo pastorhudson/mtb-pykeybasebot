a2585c2bb20e1beb40004b56b0ff6b2aa541dc10
Ron Hudson <pastorhudson@users.noreply.github.com>
2024-02-16 10:55:17 -0500

    Comment out logging in send_queue.py
    The logging lines in botcommands/send_queue.py have been commented out. This reduces the amount of logs produced during the process of sending queued bot messages, making the logs more manageable and improving performance by reducing unnecessary log generation.

--- botcommands/send_queue.py
+++ botcommands/send_queue.py
@@ -4,17 +4,17 @@ from models import MessageQueue
 
 
 async def process_message_queue(bot):
-    logging.info("Checking if we have messages")
+    # logging.info("Checking if we have messages")
     # await bot.chat.send('0000c3e1daf296e6c893a02f6ae2e39bbe99ecfbdc7bec6daccb3fd9efb0382d', "Hello World!")
 
     try:
-        logging.info("Trying to look for messages")
+        # logging.info("Trying to look for messages")
         messages = s.query(MessageQueue).filter(MessageQueue.status == 'PENDING').all()
-        logging.info(f"{len(messages)}")
+        # logging.info(f"{len(messages)}")
         for message in messages:
-            logging.info("Iterating messages")
-            logging.info(f"{message.id}: {message.status}")
-            logging.info(f"Message status: {message.status}")
+            # logging.info("Iterating messages")
+            # logging.info(f"{message.id}: {message.status}")
+            # logging.info(f"Message status: {message.status}")
             message.mark_as_processing(s)
             if message.sender != "None":
                 msg = f"Incoming Transmission from {message.user.username} via {message.sender}: {message.message}"
@@ -23,9 +23,9 @@ async def process_message_queue(bot):
 
             await bot.chat.send(message.destination, msg)
             message.mark_as_done(s)
-        logging.info("All messages processed")
+        # logging.info("All messages processed")
         return
 
     except Exception as e:
         logging.info(f"Error: {e}")
-    logging.info("Running message queue")
+    # logging.info("Running message queue")
