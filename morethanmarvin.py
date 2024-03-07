1361375084f54d376dbf09efc284bd07aa665c51
Ron Hudson <pastorhudson@users.noreply.github.com>
2024-03-07 16:29:54 -0500

    Fix index in Morse Code translation command
    The substring index in the '!morse' command within the main bot script was adjusted. Originally, the bot was not correctly translating the input due to an incorrect starting index of the input text for the Morse code conversion function. The starting index was corrected to include the right characters for accurate Morse code translation.

--- morethanmarvin.py
+++ morethanmarvin.py
@@ -767,7 +767,7 @@ async def handler(bot, event):
     if str(event.msg.content.text.body).startswith('!morse'):
         conversation_id = event.msg.conv_id
 
-        morse_code = get_morse_code(event.msg.content.text.body[6:])
+        morse_code = get_morse_code(event.msg.content.text.body[7:])
 
         await bot.chat.send(conversation_id, morse_code)
 
