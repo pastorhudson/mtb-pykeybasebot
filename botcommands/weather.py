599ccc40d04ccf073fc96924fffca75601dae6e8
Ron Hudson <pastorhudson@users.noreply.github.com>
2024-03-08 10:21:50 -0500

    Refactor get_weather function to return message string
    Changed the get_weather function in 'weather.py' to create a message string instead of printing directly to the console. This allows for more flexible usage of the function, enabling the message to be printed out or passed to other parts of the bot's code as needed.

--- botcommands/weather.py
+++ botcommands/weather.py
@@ -4,19 +4,21 @@ from datetime import date, timedelta
 
 
 def get_weather(name, LATLONG):
+    msg = ""
     weekday = date.today()
     with forecast(os.environ.get('PIRATE_WEATHER'), *LATLONG) as place:
-        print(f"{name}n: {place.daily.summary}\n---\n")
+        msg += f"{name}n: {place.daily.summary}\n---\n"
         for day in place.daily:
             day = dict(day=date.strftime(weekday, '%a'),
                        sum=day.summary,
                        tempMin=day.temperatureMin,
                        tempMax=day.temperatureMax
                        )
-            print('{day}: {sum} Temp range: {tempMin} - {tempMax}'.format(**day))
+            msg +='{day}: {sum} Temp range: {tempMin} - {tempMax}\n'.format(**day)
             weekday += timedelta(days=1)
+    return msg
 
 
 if __name__ == '__main__':
     LATLONG = 39.90008, -79.71643
-    get_weather("Uniontown", (39.90008, -79.71643))
+    print(get_weather("Uniontown", (39.90008, -79.71643)))
