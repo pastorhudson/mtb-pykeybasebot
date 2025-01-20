2acf09bf4b14224276e1499b48d8734b7c1f45c4
Ron Hudson <pastorhudson@users.noreply.github.com>
2025-01-20 17:08:53 -0500

    Update SQLAlchemy version and adjust Alembic configurations
    Upgraded SQLAlchemy to version 2.0.37 in `requirements.txt` and updated Alembic DB URL handling to support `postgresql+psycopg` scheme. Added a new Alembic migration file with placeholders for future schema changes. These changes ensure compatibility and better support for PostgreSQL.

--- alembic/env.py
+++ alembic/env.py
@@ -14,12 +14,14 @@ sys.path = ['', '..'] + sys.path[1:]
 config = context.config
 
 # SET DB_URL
-# config.set_main_option('sqlalchemy.url', os.environ.get('DATABASE_URL'))
-# SET DB_URL
 db_url = os.environ.get('DATABASE_URL')
-if db_url and db_url.startswith('postgres://'):
-    db_url = db_url.replace('postgres://', 'postgresql://', 1)
+if db_url:
+    if db_url.startswith('postgres://'):
+        db_url = db_url.replace('postgres://', 'postgresql+psycopg://', 1)
+    elif db_url.startswith('postgresql://') and not db_url.startswith('postgresql+psycopg://'):
+        db_url = db_url.replace('postgresql://', 'postgresql+psycopg://', 1)
 config.set_main_option('sqlalchemy.url', db_url)
+
 
 
 # Interpret the config file for Python logging.
