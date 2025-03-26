82737e57bcadd27246bab5e57cba71b4feb99e78
Ron Hudson <pastorhudson@users.noreply.github.com>
2025-03-25 23:08:11 -0400

    Add async function to download images via URL
    Introduced `download_image` function using `aiohttp` for asynchronous HTTP requests. This function saves images locally and ensures robust handling of HTTP responses and file storage. Also added necessary imports (`Path` and `aiohttp`) to support the feature.

--- botcommands/utils.py
+++ botcommands/utils.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+
+import aiohttp
+
 from crud import s
 from models import Team, User
 
@@ -13,3 +17,23 @@ def get_team(team_name):
     team = s.query(Team).filter_by(name=team_name).first()
 
     return team
+
+
+async def download_image(pic_url, file_name='meh.png'):
+    storage = Path('./storage')
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(pic_url) as response:
+            if response.status != 200:
+                return None
+
+            file_path = f"{storage.absolute()}/{file_name}"
+
+            with open(file_path, 'wb') as file:
+                while True:
+                    chunk = await response.content.read(1024)
+                    if not chunk:
+                        break
+                    file.write(chunk)
+
+            return file_path
