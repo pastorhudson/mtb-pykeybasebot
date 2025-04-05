80bb9c71591c2dd1e6517d8f6744b532b0897cf2
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2025-04-05 17:04:42 -0400

    Fix recipient validation and adjust max points condition
    Updated recipient validation to ensure accurate sender comparison and modified the condition to check for points strictly greater than the maximum allowed. This improves logic clarity and prevents incorrect behavior.

--- botcommands/scorekeeper.py
+++ botcommands/scorekeeper.py
@@ -134,11 +134,11 @@ async def award(bot, event, sender, recipient, team_members, points, description
 
         if user in team_members:
             logging.info("User is in members")
-        if user == event.msg.sender.username:
+        if recipient == event.msg.sender.username:
             logging.info(f"{event.msg.sender.username} is {user}")
             return f"@{user} you can't give points to yourself. How incredibly sad."
-        if points >= pts_max:
-            logging.info(f"{points} is greater than or equal to {pts_max}")
+        if points > pts_max:
+            logging.info(f"{points} is greater than {pts_max}")
             return f"@{user} you can't give more than {pts_max} points at a time. I know you know that. You know that I know that you know that."
         if recipient in team_members and recipient != event.msg.sender.username and points <= pts_max:
             score = write_score(recipient, event.msg.sender.username, team_name, points, description=description)
