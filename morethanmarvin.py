903dc5886ade37a598eeffdff8eacfdd584b4daf
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2025-04-05 16:55:29 -0400

    Fix redundant condition block for @marvn mentions.
    Removed duplicate or redundant condition logic for handling @marvn mentions. This ensures cleaner and more maintainable code without affecting functionality.

--- morethanmarvin.py
+++ morethanmarvin.py
@@ -263,12 +263,17 @@ async def handler(bot, event):
                 await sync(event=event, bot=bot)
                 await handle_marvn_mention_with_context(bot, event)
 
-
             elif (str(event.msg.content.text.body).lower().startswith("@marvn") or
                   hasattr(event.msg.content, 'at_mention_usernames') and "marvn" in
                   event.msg.content.at_mention_usernames):
                 await sync(event=event, bot=bot)
                 await handle_marvn_mention_with_context(bot, event)
+
+        elif (str(event.msg.content.text.body).lower().startswith("@marvn") or
+              hasattr(event.msg.content, 'at_mention_usernames') and "marvn" in
+              event.msg.content.at_mention_usernames):
+            await sync(event=event, bot=bot)
+            await handle_marvn_mention_with_context(bot, event)
     except Exception as e:
         logging.info(e)
 
